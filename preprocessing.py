2ddf8f809a10775ce1ac2daa0db2b80e5205e378
fhesna <fatmahesnabasari@gmail.com>
2026-06-16 16:34:53 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,44 @@
 import tensorflow as tf
-import os
-
-
-image_dataset_from_directory = tf.keras.utils.image_dataset_from_directory
-layers = tf.keras.layers
+from tensorflow.keras import layers
 
 def get_datasets(train_dir, val_dir, image_size, batch_size, model_type):
+
     data_augmentation = tf.keras.Sequential([
         layers.RandomFlip("horizontal"),
         layers.RandomRotation(0.2),
         layers.RandomZoom(0.2),
         layers.RandomContrast(0.2),
-        layers.RandomHeight(0.2),
-        layers.RandomWidth(0.2),
-    ], name ="data_augmentation")
+    ], name="data_augmentation")
+
     if model_type == 'MobileNetV2':
         preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
     elif model_type == 'EfficientNetB1':
         preprocess_input = tf.keras.applications.efficientnet.preprocess_input
     elif model_type == 'ResNet50':
-        preprocess_input = tf.keras.applications.resnet50.preprocess_input
+        preprocess_input = tf.keras.applications.resnet.preprocess_input
     else:
-        raise ValueError(f"Unknown model type: {model_type}")
-    train_ds = image_dataset_from_directory(
+        raise ValueError("Unknown model type")
+
+    train_ds = tf.keras.utils.image_dataset_from_directory(
         train_dir,
         image_size=image_size,
         batch_size=batch_size,
-        label_mode= 'int'
+        label_mode='categorical'
     )
-    val_ds =image_dataset_from_directory(
+
+    val_ds = tf.keras.utils.image_dataset_from_directory(
         val_dir,
         image_size=image_size,
         batch_size=batch_size,
-        label_mode= 'int'
+        label_mode='categorical'
     )
-    train_ds = train_ds.map(lambda x, y: (preprocess_input(data_augmentation(x, training=True)), y))
-    val_ds = val_ds.map(lambda x,y: (preprocess_input(x), y))
+
+    train_ds = train_ds.map(
+        lambda x, y: (preprocess_input(data_augmentation(x, training=True)), y)
+    )
+
+    val_ds = val_ds.map(
+        lambda x, y: (preprocess_input(x), y)
+    )
+
     return train_ds, val_ds
