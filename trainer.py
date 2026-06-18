03bbc11067a6ec1b62d889dcf4995612531808b1
fhesna <fatmahesnabasari@gmail.com>
2026-06-18 12:50:27 +0300

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -16,6 +16,6 @@ class Trainer:
         history = self.model.fit(train_data, 
                                  validation_data=validation_data, 
                                  epochs=self.epoch,                                 
-                                 callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)]
+                                 callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=7, restore_best_weights=True)]
 )
         return history
